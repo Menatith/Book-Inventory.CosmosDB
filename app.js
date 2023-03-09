4fe2694549182e0df406b9473da1d99e13750d74
Marjolein <mse.bente@gmail.com>
2023-03-09 11:45:35 +0100

    Adds select option to series column

--- app.js
+++ app.js
@@ -126,6 +126,7 @@ const view = (() => {
                 base_path: 'node_modules/tablefilter/dist/tablefilter/',
                 col_4: 'select',
                 col_6: 'select',
+                col_16: 'select',
                 highlight_keywords: true,
                 rows_counter: true,
                 col_types: [
